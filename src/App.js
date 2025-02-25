098ec044f5b46c4d452212b67ead31da68a980b6
Anurag Kushwaha <ak4032777@gmail.com>
2025-02-25 11:38:08 +0530

    added initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import UploadForm from './components/UploadForm';
+import RetrieveForm from './components/RetrieveForm';
+import SearchByNameForm from './components/SearchByNameForm';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container grid grid-col-1 md:grid-cols-3 p-4">
+      <UploadForm />
+      <SearchByNameForm />
+      <RetrieveForm />
+     
     </div>
   );
-}
+};
 
 export default App;
